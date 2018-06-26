2fd31ad28781633707bdf3d8d02dd4f29cf8a604
Nick Cox <nick@nickcox.me>
2018-06-25 22:52:05 -0700

    Most of basic functionality to spec

--- src/index.js
+++ src/index.js
@@ -4,8 +4,10 @@ import ReactDOM from "react-dom";
 import "./styles.css";
 
 const KEYBOARD_KEYS = {
-  'esc': 27,
-  'enter': 13,
+  esc: 27,
+  enter: 13,
+  rightArrow: 39,
+  leftArrow: 37
 };
 
 class App extends Component {
@@ -17,6 +19,7 @@ class App extends Component {
   }
 
   static getMapping() {
+    // TODO toUpperCase()
     return {
       A: ["À", "Â", "Æ"],
       C: ["Ç"],
@@ -27,7 +30,7 @@ class App extends Component {
       a: ["à", "â", "æ"],
       c: ["ç"],
       e: ["é", "è", "ë", "ê"],
-      i: ["î, ï"],
+      i: ["î", "ï"],
       o: ["ô", "œ"],
       u: ["ù", "û", "ü"]
     };
@@ -40,27 +43,45 @@ class App extends Component {
     this.hidePicker = this.hidePicker.bind(this);
     this.insertCharacter = this.insertCharacter.bind(this);
     this.conditionallyHidePicker = this.conditionallyHidePicker.bind(this);
+    this.reset = this.reset.bind(this);
     this.timer = null;
     this.myInput = React.createRef();
     this.state = {
       showPicker: false,
       characters: [],
-      lastKeypress: undefined
+      lastKeypress: undefined,
+      buttonFocusIndex: 0
     };
   }
 
   componentDidMount() {
     document.addEventListener("click", this.hidePicker);
-    document.addEventListener('keydown', this.conditionallyHidePicker);
+    document.addEventListener("keydown", this.conditionallyHidePicker);
+    document.addEventListener("keyup", this.reset);
   }
 
   componentWillUnmount() {
     document.removeEventListener("click", this.hidePicker);
-    document.removeEventListener('keydown', this.conditionallyHidePicker);
+    document.removeEventListener("keydown", this.conditionallyHidePicker);
+    document.removeEventListener("keyup", this.reset);
   }
 
   conditionallyHidePicker(e) {
+    if (!this.state.showPicker) return;
+    console.log(this.state.buttonFocusIndex);
     if (e.which === KEYBOARD_KEYS.esc) this.setState({ showPicker: false });
+    if (
+      e.which === KEYBOARD_KEYS.leftArrow &&
+      this.state.buttonFocusIndex > 0
+    ) {
+      this.setState({ buttonFocusIndex: this.state.buttonFocusIndex - 1 });
+    }
+    if (
+      e.which === KEYBOARD_KEYS.rightArrow &&
+      this.state.buttonFocusIndex < this.state.characters.length - 1
+    ) {
+      this.setState({ buttonFocusIndex: this.state.buttonFocusIndex + 1 });
+    }
   }
 
   ensureCharsEntered(e) {
@@ -75,22 +96,44 @@ class App extends Component {
     }
   }
 
+  reset() {
+    this.timer = null;
+    clearTimeout(this.timer);
+    this.setState({
+      lastKeypress: null,
+      buttonFocusIndex: 0
+    });
+  }
+
   insertCharacter(char) {
     const input = this.myInput.current;
-    let text = input.value.split();
-    text[App.getCursorIndex(input) - 1] = char;
-    input.value = text.join();
+    const cursorPosition = App.getCursorIndex(input);
+    let text = input.value.split('');
+    text[cursorPosition - 1] = char;
+    input.value = text.join('');
+    input.focus();
+    input.setSelectionRange(cursorPosition, cursorPosition);
+    this.setState({ buttonFocusIndex: 0 });
   }
 
   showPicker(e) {
+    if (e.which === KEYBOARD_KEYS.rightArrow) {
+      this.setState({ buttonFocusIndex: this.state.buttonFocusIndex + 1 });
+      return;
+    }
+    if (e.which === KEYBOARD_KEYS.leftArrow) {
+      this.setState({ buttonFocusIndex: this.state.buttonFocusIndex - 1 });
+      return;
+    }
     const target = e.target;
     if (e.which === this.state.lastKeypress) {
       e.preventDefault();
       if (!this.timer) {
         this.timer = setTimeout(() => {
-          const typedChar = target.value.split('')[
+          const typedChar = target.value.split("")[
             App.getCursorIndex(target) - 1
           ];
+          console.log(typedChar);
           if (App.getMapping()[typedChar]) {
             this.setState({
               showPicker: true,
@@ -111,7 +154,7 @@ class App extends Component {
       <div className="App">
         <input
           onKeyDown={this.showPicker}
-          onKeyUp={this.ensureCharsEntered}
+          //onKeyUp={this.ensureCharsEntered}
           type="text"
           ref={this.myInput}
         />
@@ -120,7 +163,13 @@ class App extends Component {
             <ul>
               {this.state.characters.map((char, index) => (
                 <li key={char}>
-                  <button onClick={() => this.insertCharacter(char)}>
+                  <button
+                    className={
+                      this.state.buttonFocusIndex === index ? "focused" : ""
+                    }
+                    autoFocus={index === 0}
+                    onClick={() => this.insertCharacter(char)}
+                  >
                     <span>{char}</span>
                     <span>{index + 1}</span>
                   </button>
